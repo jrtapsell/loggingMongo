c4266511611672a0c3cb14689c229a6c1e6d1f36
James Tapsell <email@jrtapsell.co.uk>
2017-07-15 17:46:45 +0100

    Improved logging

--- makeServer.sh
+++ makeServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp -r template servers/$1
 echo "  port: $2" >> servers/$1/mongod.conf
-echo "../../mongod -f mongod.conf | tee audit/mirror.txt | nc -lp $3" >> servers/$1/run.sh
+echo "echo Running server $1" >> servers/$1/run.sh
+echo "../../mongod -f mongod.conf | tee audit/mirror.txt | tee /dev/tty | nc -lp $3" >> servers/$1/run.sh
