0ba2969a4970ee16a6d9313c1cabd69fe83e9c51
James Tapsell <email@jrtapsell.co.uk>
2017-07-17 14:44:49 +0100

    Improved logging of created servers

--- makeServer.sh
+++ makeServer.sh
@@ -3,9 +3,9 @@ cp -r template servers/$1
 cd servers/$1
 echo "  port: $2" >> mongod.conf
 echo "echo Running server $1" >> run.sh
-echo "../../mongod -f mongod.conf --auth | tee audit/mirror.txt | tee /dev/tty | nc -lp $3" >> run.sh
+echo "../../mongod -f mongod.conf --auth | tee -a audit/mirror.log | tee /dev/tty | nc -lp $3" >> run.sh
 
-../../mongod -f mongod.conf > /dev/null &
+../../mongod -f mongod.conf >> audit/mirror.log &
 export mongoServ=$!
 sleep 5
 mongo -port $2 < ../../makeUser.js > /dev/null
