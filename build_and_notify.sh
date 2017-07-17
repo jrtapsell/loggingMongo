cc2b3b9356c6d3fc793af1be97e84f6286e98a5f
James Tapsell <email@jrtapsell.co.uk>
2017-07-17 13:43:47 +0100

    Made notify send slack notifications

--- build_and_notify.sh
+++ build_and_notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+curl -X POST --data-urlencode 'payload={"channel": "#mongo_build", "text": "Build Started from commit '$(git rev-parse HEAD)'"}' https://hooks.slack.com/services/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
 ./build.sh &&
-notify-send "Build complete" "Custom server running" &&
-aplay ~/Downloads/Pling-KevanGC-1485374730.wav
+curl -X POST --data-urlencode 'payload={"channel": "#mongo_build", "text": "Build Completed"}' https://hooks.slack.com/services/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
